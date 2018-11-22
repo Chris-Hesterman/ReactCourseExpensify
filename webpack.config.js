b8696a73fa9e32d32b5d6e8821a8f9882ed4bf5e
Chris Hesterman <spoiler14@comcast.net>
2018-11-22 15:22:07 -0800

    modified package.json and webpack.config for productionmode

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
 //entry point --> output
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path:path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
+module.exports = (env) => {
+    const isProduction = env==='production';
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path:path.join(__dirname, 'public'),
+            filename: 'bundle.js'
         },
-        {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-sourcemap',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-};
+        mode: 'development',
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-sourcemap',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
+}
