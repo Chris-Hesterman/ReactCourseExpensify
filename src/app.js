d0aad94feddcd6b9d9cc083d2737062ce16df17d
Chris Hesterman <spoiler14@comcast.net>
2018-11-21 18:42:11 -0800

    remove dispatches, selector from app.js

--- src/app.js
+++ src/app.js
@@ -11,37 +11,12 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-
-store.dispatch(addExpense({
-    description: 'water bill',
-    amount: 300,
-    note: 'expensive',
-    createdAt: 4300
-}));
-
-store.dispatch(addExpense({
-    description: 'gas bill',
-    amount: 400,
-    createdAt: 5000
-}));
-
-store.dispatch(addExpense({
-    description: 'cable',
-    amount: 2600,
-    createdAt: -1000,
-    note: 'Comcast'
-}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses, state);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
 );
-
 const app = document.querySelector("#app");
+
 ReactDOM.render(jsx, app);
 
