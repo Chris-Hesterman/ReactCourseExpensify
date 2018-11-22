e48ecfbc24cf67c41088be4be9f31fb9fe03a46c
Chris Hesterman <spoiler14@comcast.net>
2018-11-22 14:39:03 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
